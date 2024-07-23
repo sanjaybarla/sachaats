e9a78093a96606792ccee184b6a7333bf42a00b8
Barla Sanjay <106946648+sanjaybarla@users.noreply.github.com>
2024-07-23 11:11:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,11 +7,7 @@ import docx2txt
 from PyPDF2 import PdfReader
 from spacy.cli import download
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 
 def read_pdf_text(uploaded_file):
